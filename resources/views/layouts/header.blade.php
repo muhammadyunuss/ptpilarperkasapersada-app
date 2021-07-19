87d3ec7113271e8d7166353ef46897950079aacb
ME-VS-EVERYBODY\ASUS <ilhamfikhriabd@gmail.com>
2021-07-19 10:01:54 +0700

    edit header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -15,7 +15,6 @@
                 </div>
                 <div class="nav-right d-inline-flex flex-wrap">
                     <ul class="d-flex flex-wrap mb-0 list-unstyled">
-                        <li><a class="search-btn" href="javascript:void(0);" title=""><i class="icon-search"></i></a></li>
                         <li class="res-menu-btn-wrap"><a class="res-menu-btn" href="javascript:void(0);" title=""><i class="fas fa-align-justify"></i></a></li>
                     </ul>
                 </div>
