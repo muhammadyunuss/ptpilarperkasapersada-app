d64fd3d0801ce6c47ac20d6438d0ac085eaa1f9b
muhammadyunuss <yunnusmuhammad@gmail.com>
2021-08-03 09:37:06 +0700

    visi misi

--- resources/views/visi_misi.blade.php
+++ resources/views/visi_misi.blade.php
@@ -8,13 +8,13 @@
                 <div class="page-top-inner d-flex flex-wrap align-items-center position-relative w-100">
                     <div class="page-title-info position-relative">
                         <i class="position-absolute"><svg viewBox="0 0 359 354" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M358.5 351H323V300.316L250.863 264.339V331.541H278.904V170.438L201.566 91.0182L200.661 327.921L223.727 351L224.179 71.1066L166.063 3V142.381L126.489 156.862L149.781 267.507L171.49 289.455V212.75H115.635L80.3576 331.541H171.49V306.651H34V351H0" stroke="#f1f1f1" stroke-width="2"/></svg></i>
-                        <h1 class="mb-0  thm-clr">Dewan Komisaris</h1>
+                        <h1 class="mb-0  thm-clr">Visi dan Misi</h1>
                         <ol class="breadcrumb mb-0 list-unstyled">
                             <li class="breadcrumb-item"><a href="index.html" title=""><i class="icon-home"></i>Beranda</a></li>
-                            <li class="breadcrumb-item active">Struktur Organisasi</li>
+                            <li class="breadcrumb-item active">Visi Misi</li>
                         </ol>
                     </div>
-                    <div class="page-title-img position-absolute"><div class="page-title-img-inner d-block w-100" style="background-image: url({{ asset('front') }}/assets/images/page-title-bg3.jpg);"></div><span class="position-absolute sec-watermark center-watermark overlap-btm-shap88 font-200">DewanKomisaris</span></div>
+                    <div class="page-title-img position-absolute"><div class="page-title-img-inner d-block w-100" style="background-image: url({{ asset('front') }}/assets/images/page-title-bg3.jpg);"></div><span class="position-absolute sec-watermark center-watermark overlap-btm-shap88 font-200">VisiMisi</span></div>
                 </div>
             </div>
         </div><!-- Page Top Wrap -->
@@ -33,13 +33,13 @@
                         <div class="founder-desc-wrap w-100">
                             <div class="sec-title w-100">
                                 <div class="sec-title-inner d-inline-block">
-                                    <span class="d-block thm-clr">Dewan Komisaris</span>
+                                    <span class="d-block thm-clr">Visi dan Misi</span>
                                     <h2 class="mb-0">PT PILARPERKASA PERSADA<i></i></h2>
                                 </div>
                             </div><!-- Sec Title -->
                             <div class="founder-desc w-100">
                                 <blockquote class="blockquote position-relative"><div class="blockquote-footer position-relative">Visi</div><i class="lft-qut icon-quote-left thm-clr position-absolute"></i><p class="mb-0">Menjadi perusahaan dibidang penyedia barang dan jasa, earth work, general kontraktor dan supplier dengan ditunjang total quality management yang memberikan total service bagi pengguna jasa diseluruh Indonesia</p><i class="rgt-qut icon-quote-right thm-clr position-absolute"></i></blockquote>
-                            
+
                                 <blockquote class="blockquote position-relative"><div class="blockquote-footer position-relative">Misi</div><i class="lft-qut icon-quote-left thm-clr position-absolute"></i><p class="mb-0">1.	Memberikan pelayanan, mutu dan kepuasan yang terbaik kepada pelanggan</p><p class="mb-0">2.	Membangun serta menciptakan citra terbaik perusahaan</p><p class="mb-0">3.	Turut berpartisipasi dalam pembangunan negara kesatuan republik Indonesia</p><i class="rgt-qut icon-quote-right thm-clr position-absolute"></i></blockquote>
                            </div>
                         </div>
